eaa11e647b2654e09e50335434eb11ddaf62f500
surysanchez <surysanchezsales@gmail.com>
2023-05-01 07:59:09 -0400

    WIP add extra feautures

--- placescollector/settings.py
+++ placescollector/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q&x!bva!!7ypxf(4$d7bzyk942qb5qt7gb%8dnxin!$mt@&#ed'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
